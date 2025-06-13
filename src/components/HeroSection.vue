b70db6aac6720c65d7dd7f69301c3da96c1aec33
unknown <nadaalbaadani669@gmail.com>
2025-06-13 12:36:36 +0300

    setting up the project

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,29 @@
+<template>
+    <section
+    class="relative bg-cover bg-center min-h-screen flex items-center justify-center text-white px-6"
+    style="background-image: url('https://i.imgur.com/DQG2iPL.png');"
+  >
+    <div class="absolute inset-0 bg-black bg-opacity-50"></div>
+
+    <div class="relative z-10 text-center max-w-3xl">
+      <h1 class="text-4xl md:text-5xl font-bold mb-4 leading-tight">
+        Creative Home Simplify Your Furniture
+      </h1>
+      <p class="text-lg text-gray-200 mb-6">
+        Do i have consent to record this meeting gain location, root-and-branch,
+        review, nor game plan who’s the goto
+      </p>
+      <a href="#" class="bg-yellow-400 text-gray-900 font-semibold px-6 py-3 rounded hover:bg-yellow-300 transition">
+        Shop Now
+      </a>
+    </div>
+  </section>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+     <header class="flex justify-between items-center px-6 py-4 text-white absolute top-0 left-0 w-full z-10">
+        <h1 class="text-2xl font-bold">FurniShop</h1>
+        <nav class="space-x-6 font-medium">
+        <a href="#" class="hover:text-gray-300">Home</a>
+        <a href="#" class="hover:text-gray-300">About</a>
+        <a href="#" class="hover:text-gray-300">Features</a>
+        <a href="#" class="hover:text-gray-300">Contact</a>
+        </nav>
+  </header>  
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
